dfb4f3e2e0d99e50c7bbbd2b905b181cde3713df
Bernardo Santos <bernardosantos670@gmail.com>
2019-09-25 20:22:07 +0100

    Solved tp01 ex07

---
+++ tp01/ex07.pl
@@ -0,0 +1,11 @@
+error_code(1).
+error_code(2).
+error_code(3).
+
+error_message(integer_overflow).
+error_message(division_by_zero).
+error_message(unknown_id).
+
+error_info(1, integer_overflow).
+error_info(2, division_by_zero).
+error_info(3, unknown_id).
