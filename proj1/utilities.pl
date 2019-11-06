39e3d5dcb35fb976e9f6343d6d7373b790b17752
Bernardo Santos <bernardosantos670@gmail.com>
2019-11-06 01:30:29 +0000

    Implemented some auxiliary terms to handle list operations

---
+++ proj1/utilities.pl
@@ -0,0 +1,30 @@
+% TODO: document this code
+
+getListItem([Item|_], 0, Item).
+getListItem([_|List], Index, Item) :-
+    Index > 0,
+    NewIndex is Index - 1,
+    getListItem(List, NewIndex, Item).
+
+
+setListItem([_|T], 0, Item, [Item|T]).
+setListItem([H|T], Index, Item, [H|RT]) :-
+    Index > 0,
+    NewIndex is Index - 1,
+    setListItem(T, NewIndex, Item, RT).
+
+
+getMatrixItem([H|_], 0, Column, Item) :-
+    getListItem(H, Column, Item).
+getMatrixItem([_|Matrix], Row, Column, Item) :-
+    Row > 0,
+    NewRow is Row - 1,
+    getMatrixItem(Matrix, NewRow, Column, Item).
+
+
+setMatrixItem([H|T], 0, Column, Item, [NewH|T]) :-
+    setListItem(H, Column, Item, NewH).
+setMatrixItem([H|T], Row, Column, Item, [H|NewT]) :-
+    Row > 0,
+    NewRow is Row - 1,
+    setMatrixItem(T, NewRow, Column, Item, NewT).
