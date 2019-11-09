f023cecdcaf3249d2512c45b5f3b39670c1951c9
Bernardo Santos <bernardosantos670@gmail.com>
2019-11-09 14:37:40 +0000

    Refactored the code to do the axial symmetry and implemented point symmetry

--- proj1/symmetry.pl
+++ proj1/symmetry.pl
@@ -29,10 +29,19 @@ axialSymmetry(Board, Orientation, AxisIndex, Positions, NewBoard) :-
 
 % Calculate the symmetric coordinate to Coord in relation to Axis
 % When the axis index is greater or equal to the coord
-calcSymmetryCoord(Coord, Axis, 1, NewCoord) :-
+blockAxialSymmetry(Coord, Axis, NewCoord) :-
+    Coord >= Axis,
     NewCoord is Coord + (Axis - Coord) * 2 + 1.
 % When the axis index is smaller than the coord
-calcSymmetryCoord(Coord, Axis, -1, NewCoord) :- 
+blockAxialSymmetry(Coord, Axis, NewCoord) :-
     NewCoord is Coord + (Coord - Axis - 1) * (-2) - 1.
 
 
+% Calculate the symmetric coordinates to Coords in relation
+% to the point that is the intersection of HAxis and VAxis
+blockPointSymmetry(Coords, HAxis, VAxis, NewCoords) :-
+    getListItem(Coords, 0, Row),
+    blockAxialSymmetry(Row, HAxis, NewRow),
+    getListItem(Coords, 1, Col),
+    blockAxialSymmetry(Col, VAxis, NewCol),
+    append([NewRow], [NewCol], NewCoords).
