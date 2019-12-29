3e0d293dba926bba37524c2687fd5235235b8aad
Bernardo Santos <bernardosantos670@gmail.com>
2019-12-29 22:55:37 +0000

    Documented solver.pl

--- proj2/code/solver.pl
+++ proj2/code/solver.pl
@@ -1,3 +1,8 @@
+% Solves the puzzle with the corresponding number, prints the solution 
+% and the execution time and returns the solved board
+% 
+% Number - number of the puzzle to solve
+% Board - solved puzzle board
 solve_puzzle(Number, Board) :-
     puzzle(Number, Col, Row),
     format('~nPuzzle Nr ~d~n~n', [Number]),
@@ -8,6 +13,12 @@ solve_puzzle(Number, Board) :-
     Time is End - Start,
     format('~nExecution time : ~3d s~n~n', [Time]).
 
+% Solves a puzzle with the provided hints
+% 
+% LOptions - labeling options
+% Col - list of column hints
+% Row - list of row hints
+% Board - solution for the puzzle
 solve_puzzle(LOptions, Col, Row, Board) :-
     length(Col, ColSize),
     length(Row, RowSize),
@@ -17,20 +28,27 @@ solve_puzzle(LOptions, Col, Row, Board) :-
     append(Board, FlatBoard),
     domain(FlatBoard, 0, 1),
     
+    % iterate through the rows and apply the restrictions
     itr_line(Row, Board, RowSize, (-2)-(-2)),
     transpose(Board, TBoard),
+    % iterate through the columns and apply the restrictions
     itr_line(Col, TBoard, ColSize, (-2)-(-2)),
     
     labeling(LOptions, FlatBoard).
 
+% Iterate through the board and apply restrictions
 itr_line([], [], _Length, _Pos).
 itr_line([HRow|TRow], [HBoard|TBoard], Length, InPos) :-
     NumZeros is Length - 2,
+    % each line can only contain two 1s and the rest must be filled with 0s
     global_cardinality(HBoard, [0-NumZeros, 1-2]),
     space_rest(HRow, HBoard, InPos, OutPos),
     itr_line(TRow, TBoard, Length, OutPos).
 
-
+% In each line the 1s must have at least one 0 between them, however when a 
+% distance is defined on the hints list that restriction is applied. Also, 
+% each 1 must be surrounded by 0s. 
+% 
 % Space - spacing between the 1s
 % Line - line that the restrictions will be applied to
 % InPos - tuple containing the positions of the 1s on the previous line
@@ -52,6 +70,14 @@ space_rest(Space, Line, PrvPos, OutPos) :-
 
     surround_rest(CurIPos-CurFPos, PrvPos, OutPos).
 
+% Each 1 must be surrounded by 0s. This predicate applies this restriction 
+% to the current line by calculating the positions in which the 1s cannot 
+% be placed in through the positions where the 1s of the previous lines 
+% where placed
+% 
+% CurPos - tuple containing the positions of the 1s for the current line
+% PrvPos - tuple containing the positions of the 1s on the previous line
+% OutPos - tuple containing the final positions for the 1s on the current line
 surround_rest(CurPos, PrvPos, OutPos) :-
     CurIPos-CurFPos = CurPos,
     PrvIPos-PrvFPos = PrvPos,
@@ -64,10 +90,14 @@ surround_rest(CurPos, PrvPos, OutPos) :-
 
     OutPos = CurIPos-CurFPos.
 
-generate_board(Cols, Rows, Board) :-
-    length_list(Rows, Board),
-    maplist(length_list(Cols), Board).
-
+% Generates an empty board, a list of lists, with the desired number 
+% of rows (number of lists) and rows (length of the lists)
+% 
+% NumCols - number of columns
+% NumRows - number of rows
+% Board - the generated board
+generate_board(NumCols, NumRows, Board) :-
+    length_list(NumRows, Board),
+    maplist(length_list(NumCols), Board).
 length_list(Int, List) :-
     length(List, Int).
-
